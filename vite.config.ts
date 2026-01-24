f7aad5bef3c44052a4decfc1cbb0a8369b3c5231
AdamMannino <amannino92@gmail.com>
2026-01-24 09:09:09 -0500

    Config Changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // GitHub Pages project site:
   // https://avmannino.github.io/public_skate_webpage_embed/
-  base: "/public_skate_webpage_embed/",
+  base: "/cosmic_skate_webpage_embed/",
 
   plugins: [react(), tailwindcss()],
   resolve: {
